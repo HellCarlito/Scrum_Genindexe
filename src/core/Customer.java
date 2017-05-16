52430f6d51dcdf8b2dca68bd86abc2d322f63c42
Chloé <Chlo@172.18.22.58>
2017-05-16 14:57:00 +0200

    customer

--- src/core/Customer.java
+++ src/core/Customer.java
@@ -9,16 +9,18 @@ public class Customer
    */
   private List<Order> orders;
   private WordUtils word;
-
-  /**
-   * Name of the customer
-   */
-  private String name;
-
-  /**
-   * Address (only the town) from the customer
-   */
-  private String town;
+  
+  private String name; //Name of the customer
+  private String town;//Address (only the town) from the customer
+  private String corporate; //The corporate name of the company
+  private int phoneNumber; //The phone number of the customer
+  private int fax; //The fax of the customer
+  private int cellNumber; //The cell phone number of the customer
+  private String address; //The invoicing address of the company
+  private String invoicingName; //The invoicing contact of the company
+  private String login; //The login of the costumer for the Internet services
+  private String password; //The password of the costumer for the Internet services
+  private String email; //The email of the costumer for the Internet services
 
   public final String getName() {
     return name;
@@ -27,6 +29,70 @@ public class Customer
   public final String getTown() {
     return town;
   }
+  
+  public final String getCorporateName() {
+    return corporate;
+  }
+
+  public int getPhoneNumber() {
+    return phoneNumber;
+  }
+  
+  public int getFax() {
+	return fax;
+  }
+
+  public int getCellNumber() {
+	return cellNumber;
+  }
+  
+  public String getAddress() {
+    return address;
+  }
+
+  public String getInvoicingName() {
+	return invoicingName;
+  }
+ 
+  public final String getLogin() {
+	return login;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+  
+  public String getEmail() {
+	return email;
+  }
+  
+  public void setPhoneNumber(int p){
+	  phoneNumber =p;
+  }
+  
+  public void setFax(int f){
+	  fax =f;
+  }
+  
+  public void setCellNumber(int c){
+	  cellNumber =c;
+  }
+  
+  public void setAddress(String a){
+	  address =a;
+  }
+  
+  public void setInvoicingName(String in){
+	  invoicingName =in;
+  }
+  
+  public void setPassword(String p){
+	  password =p;
+  }
+  
+  public void setEmail(String e){
+	  email =e;
+  }
 
   /**
    * Constructor for the customer
@@ -34,10 +100,20 @@ public class Customer
    * @param n name of the customer
    * @param t town for the customer
    */
-  public Customer(String n, String t)
+  public Customer(String n, String t, String c, int p, int f, int cell, String ad, String fac,
+		  String log, String pass, String mail)
   {
 	  name = word.capitalize(n);
 	  town = word.capitalize(t);
+	  corporate = word.capitalize(c);
+	  phoneNumber = p;
+	  fax = f;
+	  cellNumber = cell;
+	  address = ad;
+	  invoicingName = fac;
+	  login = log;
+	  password = pass;
+	  email = mail;
 	  orders = new ArrayList<Order>();
   }
 
