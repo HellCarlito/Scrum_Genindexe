97da866d321c05029702117530129a0eac31ea6c
Chloé <Chlo@172.18.22.58>
2017-05-16 15:45:12 +0200

    model avec rien dedans

--- src/hci/CreateCustomer/ModelCreateCustomer.java
+++ src/hci/CreateCustomer/ModelCreateCustomer.java
@@ -1,5 +1,17 @@
 package hci.CreateCustomer;
+import java.util.ArrayList;
+import core.Customer;
+import core.CustomerBase;
 
-public class ModelCreateCustomer {
+public class ModelCreateCustomer
+{
+	private CustomerBase customerBase;
 
+    public ModelCreateCustomer()
+    {
+    }
+
+    public ArrayList<Customer> getArrayListCustomer(){
+        return customerBase.getCustomers();
+    }
 }
