2554fdb8869f656927236e9717098b2e6ce8d00b
Makamitsu <ambiehlaugustin@gmail.com>
2017-05-17 10:21:46 +0200

    addind jsp for the mainview

--- src/hci/MainWindow.java
+++ src/hci/MainWindow.java
@@ -1,23 +1,32 @@
 package hci;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * This class take in charge the creation of the context for any new panel
  */
 public class MainWindow extends JFrame {
 
+    private JScrollPane jScrollPane;
+    JScrollPane scrollpane;
+
     public MainWindow(){
         super("Genindexe");
+        jScrollPane = new JScrollPane();
+        getContentPane().add(jScrollPane);
+        getContentPane().setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public void setContent(JPanel jp){
         getContentPane().removeAll();
-        getContentPane().add(jp);
-        jp.updateUI();
-        pack();
+        jScrollPane.removeAll();
+        scrollpane = new JScrollPane(jp);
+        getContentPane().add(scrollpane, BorderLayout.CENTER);
+        jScrollPane.updateUI();
+
     }
 
 }
